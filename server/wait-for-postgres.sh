4bc1d02d067c4c5b4a32070172f83e9a9f09c092
Andrés Baez <abaez@carrasco.uruit.com>
2017-10-17 19:38:20 -0300

    delete unused files

--- server/wait-for-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-host="$1"
-until psql -h "$host" -U "postgres" -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
->&2 echo "Postgres is up - executing command"
